1c12bded13e7b10f6ddb091b6212d8cf46e8a697
Adiecirb <briceidamh@comunidad.unam.mx>
2026-05-12 19:57:14 -0600

    Implementacion de HomeActivity con BottomNavigationView y Navigation

---
+++ app/src/main/java/com/app/novabank/HomeActivity.kt
@@ -0,0 +1,25 @@
+package com.app.novabank
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.app.novabank.R
+import com.app.novabank.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHost.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+}
